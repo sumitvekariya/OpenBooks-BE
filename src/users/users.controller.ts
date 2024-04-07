59c58fb0f13eafcfb83c71af0626c59f95d2c371
sumitvekariya <sumitvekariya7@gmail.com>
2024-04-06 22:58:55 -0400

    remove required check

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -26,10 +26,10 @@ const bookSchema = Joi.array().items({
 }).min(1).max(3).required();
 
 const signupSchema = Joi.object({
-  longitude: Joi.string(),
-  latitude: Joi.string(),
-  name: Joi.string(),
-  email: Joi.string(),
+  longitude: Joi.string().allow(""),
+  latitude: Joi.string().allow(""),
+  name: Joi.string().allow(""),
+  email: Joi.string().allow(""),
   profilePicture: Joi.string(),
   books: bookSchema
 });
